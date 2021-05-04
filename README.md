b3a816e6ad584682969a05966aac1d094cf6e28a
lanblau <73086303+lanblau@users.noreply.github.com>
2021-05-05 01:44:58 +0800

    20210505_change

--- README.md
+++
@@ -1,2 +0,0 @@
-# index
-[點我](https://lanblau.github.io/index/Index.html)
--- js/walkingservice/mobile/custom.js
+++
@@ -1,7 +0,0 @@
-
-$(".n_slider").owlCarousel({
-    // loop: true,
-    // autoplay: true,
-    // autoplayTimeout: 5000, //2000ms = 2s;
-    // autoplayHoverPause: true,
-});
